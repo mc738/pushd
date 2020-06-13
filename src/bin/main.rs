6c7bd37757d2ab1a404ee0791f12f0ec7f80ce47
Max <me.mclifford.dev>
2020-06-13 21:39:45 +0100

    Made basic SCP process

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,16 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut process = Command::new("scp");
+
+    process.current_dir("[directory]");
+
+    process.arg("[file]");
+
+    process.arg("[user]@[address]:[path]");
+
+    let result = process.output().expect("Could not excute process");
+
+    println!("Output:\n{}",String::from_utf8_lossy(&result.stdout));
 }
